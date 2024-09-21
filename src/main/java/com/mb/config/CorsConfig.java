1c402690b7982da7cc91913086efed6b47f236fb
SoravRamgharia <97608302+SoravRamgharia@users.noreply.github.com>
2024-09-21 17:55:47 +0530

    Update CorsConfig.java

--- src/main/java/com/mb/config/CorsConfig.java
+++ src/main/java/com/mb/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.mb.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+	
+	@Value("${host.url}")
+	private String hostUrl;
+
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -14,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://mb-production.up.railway.app")
+                        .allowedOrigins(hostUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
