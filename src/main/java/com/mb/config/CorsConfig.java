44c3bd6f3f1b692faba81ea79841071f36321d11
SoravRamgharia <97608302+SoravRamgharia@users.noreply.github.com>
2024-09-21 17:20:28 +0530

    Update CorsConfig.java

--- src/main/java/com/mb/config/CorsConfig.java
+++ src/main/java/com/mb/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080")
+                        .allowedOrigins("https://mb-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
