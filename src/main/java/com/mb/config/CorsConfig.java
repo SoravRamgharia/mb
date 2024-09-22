e48d60a102b79804c8191daa05be2efce6119fc3
SoravRamgharia <97608302+SoravRamgharia@users.noreply.github.com>
2024-09-22 18:05:18 +0530

    Update CorsConfig.java

--- src/main/java/com/mb/config/CorsConfig.java
+++ src/main/java/com/mb/config/CorsConfig.java
@@ -8,22 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-	
-	@Value("${host.url}")
-	private String hostUrl;
 
+	@Value("${server.baseUrl}")
+	private String BASE_URL;
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(hostUrl)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(BASE_URL)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
