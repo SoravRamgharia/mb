cfe25bb0a58298d6aaf029ad0aed84aa5a5b563b
SoravRamgharia <97608302+SoravRamgharia@users.noreply.github.com>
2024-09-26 10:43:38 +0530

    Update MarriageBureauApplication.java

--- src/main/java/com/mb/MarriageBureauApplication.java
+++ src/main/java/com/mb/MarriageBureauApplication.java
@@ -1,5 +1,6 @@
 package com.mb;
 
+import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -26,58 +27,62 @@ import com.mb.exception.GlobalExceptionHandler;
 @EnableScheduling
 public class MarriageBureauApplication {
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(MarriageBureauApplication.class, args);
-        System.out.println("<=====: This Spring Boot Website Developed by MANPREET SINGH (9592297120) :=====>");
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MarriageBureauApplication.class, args);
+		System.out.println("<=====: This Spring Boot Website Developed by MANPREET SINGH (9592297120) :=====>");
+	}
 
-    @Bean
-    public GlobalExceptionHandler globalExceptionHandler() {
-        return new GlobalExceptionHandler();
-    }
+	@Bean
+	public GlobalExceptionHandler globalExceptionHandler() {
+		return new GlobalExceptionHandler();
+	}
 
-    @Autowired
-    private UserRepo userRepo;
+	@Autowired
+	private UserRepo userRepo;
 
-    @Autowired
-    private PasswordEncoder appPasswordEncoder;
-    
+	@Autowired
+	private PasswordEncoder appPasswordEncoder;
+
 	@Value("${admin.email}")
 	private String adminEmail;
 
+	@PostConstruct
+	public void createAdmin() throws Exception {
+		User user = new User();
+//		List<String> pic = new ArrayList<>();
+//		pic.add("img/Imagebg.png");
 
-    @PostConstruct
-    public void createAdmin() throws Exception {
-        User user = new User();
-        List<String> pic = new ArrayList<>();
-        pic.add("img/Imagebg.png");
+		List<String> imagesList = new ArrayList<>(); 
+		imagesList.add("img/Imagebg.png");
 
-        user.setName("Admin");
-        user.setEmail(adminEmail);
-        user.setPassword(appPasswordEncoder.encode("open"));
-        user.setRoleList(List.of(AppConstants.ROLE_USER));
-        user.setPicture(pic.get(0));
-        user.setImages(pic);
-        user.setGender("Male");
-        user.setReligion("Sikh");
-        user.setCaste("ramgharia");
-        user.setDateOfBirth("15/11/1999");
-        user.setHeight(6.6);
-        user.setPlace("indian");
-        user.setMarriedStatus("Married");
-        user.setQualification("Under-Graduate");
-        user.setOccupation("Business");
-        user.setFamilyStatus("Moderate");
-        user.setTotalFamilyMembers(5);
-        user.setTotalBrothers(2);
-        user.setTotalSisters(1);
-        user.setPhoneNumber1("1234567890");
-        user.setFormFilledBy("Self");
-        
-        userRepo.findByEmail(adminEmail).ifPresentOrElse(user1 -> {
-        }, () -> {
-            userRepo.save(user);
-            System.out.println("<==========: Admin has Created Succesfully :==========>");
-        });
-    }
+		user.setName("Admin");
+		user.setEmail(adminEmail);
+		user.setPassword(appPasswordEncoder.encode("open"));
+		user.setRoleList(List.of(AppConstants.ROLE_USER));
+//		user.setPicture(imagesList.get(0));   
+        // Use setImagesList to convert the list to a JSON string and set it
+        user.setImagesList(imagesList);
+     
+		user.setGender("male");
+		user.setReligion("sikh");
+		user.setCaste("ramgharia");
+		user.setDateOfBirth("16/11/2000");
+		user.setHeight(6.8);
+		user.setPlace("indian");
+		user.setMarriedStatus("Married");
+		user.setQualification("Under-Graduate");
+		user.setOccupation("business");
+		user.setFamilyStatus("moderate");
+		user.setTotalFamilyMembers(5); 
+		user.setTotalBrothers(2);
+		user.setTotalSisters(1);
+		user.setPhoneNumber1("1234567890");
+		user.setFormFilledBy("Self");
+
+		userRepo.findByEmail(adminEmail).ifPresentOrElse(user1 -> {
+		}, () -> {
+			userRepo.save(user);
+			System.out.println("<==========: Admin has Created Succesfully :==========>");
+		});
+	}
 }
